a6630392428ec85a8129bf8c7fe3eb971346e313
AnnahGerletti <agerletti@gmail.com>
2017-10-26 15:42:20 +1300

    database made and seeded

--- seeds/ledgers.js
+++ seeds/ledgers.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
       return knex('ledger').insert([
         {id: 1, contractor_id: 1, transaction_amount: 300, comment:'From a visual design contract'},
         {id: 2, contractor_id: 1, transaction_amount: -400, comment:'Weekly withdrawl'},
-        {id: 3, contractor_id: 1, transaction_amount: 500, comment:'From a visual design contract'},
+        {id: 3, contractor_id: 1, transaction_amount: 500, comment:'From a visual design contract'}
       ]);
     });
 };
