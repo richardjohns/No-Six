be9513383ea1044ff766cd7d9b261e4e7ab230bd
don-harris <donald.walter.harris@gmail.com>
2017-10-27 14:42:51 +1300

    css for home page

--- client/components/FinancePage.jsx
+++ client/components/FinancePage.jsx
@@ -14,10 +14,26 @@ class FinancePage extends React.Component {
     return (
       <div>
         <progress className="progress is-large" value="60" max={this.props.total || 100}></progress> <div className="level-right">Total:{this.props.total * 52}</div>
-        <br/>
-        <br/>
-        <a className="button is-large"><Link to="/ledger">Ledger &#8594;</Link></a><br/>
-        <a className="button is-large"><Link to="/expenses">Expenses &#8594;</Link></a>
+        <br />
+        <br />
+        <div className="tile is-ancestor">
+          <div className="tile is-4 is-vertical is-parent">
+            <div className="tile is-child box">
+              <p className="title">Ledger</p> <br/>
+              <p className="subtitle">Here you can work out your gross earnings and see how your progress bar changes ! This highlights your financial goals and process your current work. Good luck saving !</p>
+              <br />
+              <button className="button is-large"><Link to="/ledger">Ledger &#8594;</Link></button>
+            </div>
+
+            <div>
+              <div className="tile is-child box">
+                <p className="title">Expenses</p><br />
+                <p className="subtitle">Here you can input your expenses which will be calculated weekly and yearly. The yearly amount sets your living full bar total !</p>
+                <button className="button is-large"><Link to="/expenses">Expenses &#8594;</Link></button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
